c2a4594798613d8b2233f1417613680250a45d7f
addy jerry <jerryb@fyaora.com>
2025-12-04 08:03:36 +0000

    testing news blog

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <main>
       <h1 className="font-bold">Thsi is my news blog</h1>
+      <p>
+        I have been able to create the repo but I cannot push to the branch and
+        I know how to go about it becausle I know Its because of SSH KEY
+      </p>
     </main>
   );
 }
